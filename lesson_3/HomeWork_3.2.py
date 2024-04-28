d511bcd1f5ebb94ba9f793d8a07f1b47a75f7a28
naaas <nazarkorzhytsky@gmail.com>
2024-04-28 17:22:40 +0200

    Commit

--- lesson_3/HomeWork_3.2.py
+++ lesson_3/HomeWork_3.2.py
@@ -1,4 +1,4 @@
-uwu = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+uwu = [1, 2, 3, 4, 5, 6, 7, 8, 11]
 if len(uwu) > 0:
     print()   #Sigma emotion
 
--- lesson_3/HomeWork_3.3.py
+++ lesson_3/HomeWork_3.3.py
@@ -1,9 +1,6 @@
-uwu = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-if len(uwu) > 0:
-    print()   #Sigma emotion
-
-else: print("uwu is less than 1")
-
-UWU = uwu.pop()
-uwu.insert(0, UWU)
-print(uwu)
+lyst = [1,2,3,4,5,6]
+if len(lyst) == 0:
+    print([[],[]])
+elif len(lyst) % 2 == 0:
+    fanta = lyst[0:len(lyst)//2]
+    print(fanta)
